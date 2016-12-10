500b1b0c3fe8964000cac165d5fc58801be3379f
VladMatyunin <vlad.matyunin96@gmail.com>
2016-12-10 15:53:21 +0300

    Working neuron-net, todo: add execution function to hidden neuron

--- fb_oracle/Neuron.py
+++ fb_oracle/Neuron.py
@@ -8,6 +8,8 @@ class EntryNeuron:
     parameter = ""
     parameter_value = None
 
+    def set_input_value(self, value):
+        self.parameter_value = value
     def __init__(self, parameter):
         self.parameter = parameter
 
@@ -23,23 +25,33 @@ class EntryNeuron:
 # 3. The value is sent to out Neuron
 class HiddenNeuron:
     way_to_out_neuron = 0
-    function_type = None
     outer_neuron = None
+    input_data = []
 
-    def __init__(self, function_type):
-        self.function = function_type
+    def __init__(self):
+        pass
 
     def execute(self):
         print "TODO"
 
     def add_input(self, stat, cost):
         # TODO: function to integrate cost into stat value
-        self.input.append(stat)
+        print ("VALUE TO HIDDEN:", stat, cost)
+        self.input_data.append(stat)
+        self.outer_neuron.set_from_hidden(stat * cost)
 
 
 class OutNeuron:
+    final_data = []
+
     def __init__(self):
         pass
+
+    def set_from_hidden(self, value):
+        self.final_data.append(value)
+
+    def get_final_data(self):
+        return self.final_data
 
 
 def randomize_way_array(array_size, min_value, max_value):
@@ -54,11 +66,11 @@ class NeuronNet:
     hidden_neurons = []
     out_neuron = None
 
-    def __init__(self, input_number, input_parameters, hidden_number, neuron_function):
+    def __init__(self, input_number, input_parameters, hidden_number):
         self.out_neuron = OutNeuron()
         # initializing hidden neurons
         for i in range(0, hidden_number):
-            self.hidden_neurons.append(HiddenNeuron(neuron_function))
+            self.hidden_neurons.append(HiddenNeuron())
             self.hidden_neurons[i].outer_neuron = self.out_neuron
         # initializing Entry Neurons
         for i in range(0, input_number):
@@ -68,13 +80,30 @@ class NeuronNet:
         self.init_net_ways()
 
     def execute(self, input_values):
-        for neuron in self.input_neurons:
-            neuron.parameter = input_values.getKey
+        for i in range(0, len(self.input_neurons)):
+            self.input_neurons[i].set_input_value(input_values[i])
+        for x in self.input_neurons:
+            x.send_to_hidden()
+        print (self.out_neuron.get_final_data())
+
+    def get_input_parameters(self):
+        a = []
+        for i in self.input_neurons:
+            a.append(i.parameter)
+        return a
+
+    def get_from_input_to_hidden_ways(self):
+        a = []
+        for i in self.input_neurons:
+            a.append(i.hidden_neurons_way)
+        return a
 
     # initializing starting graph-like net
     def init_net_ways(self):
+        # from entry to hidden neurons
         for neuron in self.input_neurons:
             neuron.hidden_neurons_way = randomize_way_array(3, 1, 10)
 
+        # from hidden to output neuron
         for neuron in self.hidden_neurons:
             neuron.way_to_out_neuron = random.uniform(1, 10)
--- fb_oracle/main.py
+++ fb_oracle/main.py
@@ -3,17 +3,18 @@ import os.path
 import math
 import Neuron
 
+input_parameters = ["LAST_5_GAMES", "HISTORY", "PLAYERS"]
+
 DB_NAME = "database.sqlite"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "database.sqlite")
 team_parse = InformationParser.Team_Dao(db_path)
 # print team_parse.get_team_by_fullname("Arsenal").api_id
 match_parse = InformationParser.Match_Dao(db_path)
-ms = match_parse.get_last5_matches_of_team(team_parse.get_team_by_id(team_parse.get_team_by_fullname("Arsenal").api_id),
-                                           "2014-10-01")
 
 
 # Returns the array of team result, 1-win, 0-draw, -1-lose
+# gets the array of matches to research and the team to consider
 def research_matches(matches, team_id):
     result = []
     for i in matches:
@@ -21,16 +22,44 @@ def research_matches(matches, team_id):
     return result
 
 
+# converts the array of 1, 0 or -1 to a number
+# like an average
+def convert(array):
+    sum = 0
+    for i in array:
+        sum += i
+    return float(sum) / len(array)
+
+# print research_matches(ms, team_parse.get_team_by_fullname("Arsenal").api_id)
+# print convert(research_matches(ms, team_parse.get_team_by_fullname("Arsenal").api_id))
+
 # print match_parse.get_players_func(1218864)
 # print research_matches(ms, 9825)
-# print match_parse.get_statistics(team_parse.get_team_by_fullname("Arsenal").api_id,
-#                                   team_parse.get_team_by_fullname("Manchester United").api_id)
-my_neural_net = Neuron.NeuronNet(3, ["x", "y", "z"], 3, "function")
+# print convert(match_parse.get_statistics(team_parse.get_team_by_fullname("Arsenal").api_id,
+#                                   team_parse.get_team_by_fullname("Manchester United").api_id))
+# my_neural_net = Neuron.NeuronNet(3, ["x", "y", "z"], 3, "function")
+#
+#
+# def test_function(x):
+#     e = math.e
+#     for i in x:
+#         print math.tan((math.exp(2*i)-1)/(math.exp(2*i)+1))
+# x = [1, 2, 3]
+# test_function(x)
+team1_id = team_parse.get_team_by_fullname("Arsenal").api_id
+team2_id = team_parse.get_team_by_fullname("Manchester United").api_id
+history = convert(match_parse.get_statistics(team1_id, team2_id))
+last_team1_match = match_parse.get_last5_matches_of_team(team_parse.get_team_by_id(team1_id), "2014-10-01")
+last_team2_match = match_parse.get_last5_matches_of_team(team_parse.get_team_by_id(team2_id), "2014-10-01")
+k1 = convert(research_matches(last_team1_match, team1_id))
+k2 = convert(research_matches(last_team2_match, team2_id))
+k1 = float(k1)
+total_k = k1/k2
+# TODO: get match_id between Arsenal and MU
+k3 = 0.7
+neuron_net = Neuron.NeuronNet(3, input_parameters, 3)
+# print (neuron_net.get_input_parameters())
+# print (neuron_net.get_from_input_to_hidden_ways())
+a = [history,total_k,k3]
+neuron_net.execute(a)
 
-
-def test_function(x):
-    e = math.e
-    for i in x:
-        print math.tan((math.exp(2*i)-1)/(math.exp(2*i)+1))
-x = [1, 2, 3]
-test_function(x)
