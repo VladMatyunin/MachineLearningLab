349b3b80cdc2fd655589c2e9396212957b779de0
vlad-matyunin96 <vlad.matyunin96@gmail.com>
2017-10-24 23:10:28 +0300

    fixed learning

--- LinearRegression/index.py
+++ LinearRegression/index.py
@@ -1,28 +1,30 @@
 import pandas as pd
 import linear_classifier as classifier
+import math
 data_frame = pd.read_csv('train.csv')
 data_types = []
 string_data_values = []
-epsilon = 8.883
+epsilon = 3.883
 classifier = classifier.MyLinearClassifier(80)
 
 for index, row in data_frame.iteritems():
     data_types.append(row.dtype)
 for i in range(0, len(data_types)):
     string_data_values.append([])
-
-for index, row in data_frame.iterrows():
-    train_data = []
-    for j in range(0,  80):
-        if data_types[j] == object:
-            if not string_data_values[j].__contains__(row[j]):
-                string_data_values[j].append(row[j])
-            data_index = string_data_values[j].index(row[j])
-            train_data.append((data_index+1)*epsilon)
-        else:
-            train_data.append(row[j])
-    result = row[80]
-    print(classifier.get_error(train_data, result))
-    classifier.apply(train_data, result)
-
+for i in range(0, 100):
+    epoch_error = 0
+    for index, row in data_frame.iterrows():
+        train_data = []
+        for j in range(0,  80):
+            if data_types[j] == object:
+                if not string_data_values[j].__contains__(row[j]):
+                    string_data_values[j].append(row[j])
+                data_index = string_data_values[j].index(row[j])
+                train_data.append(data_index*100+epsilon)
+            else:
+                train_data.append(row[j])
+        result = row[80]
+        epoch_error += math.fabs(classifier.get_error(train_data, result)/result)
+        classifier.apply(train_data, result)
+    print(epoch_error/data_frame.size)
 print(len(data_types))
--- LinearRegression/linear_classifier.py
+++ LinearRegression/linear_classifier.py
@@ -5,36 +5,41 @@ import math
 class MyLinearClassifier:
     def __init__(self, size):
         self.period = 1
-        self.size = size+1
-        self.grad_step = 1888
-        self.params = np.random.randint(-20, 20, size=size+1)
+        self.size = size + 1
+        self.grad_step = 0.00000001
+        self.params = np.random.random_sample(size=size + 1)
+        for i in range(0, size+1):
+            self.params[i] += np.random.randint(-5, 5)
 
     def fit(self, data, result):
+        full_data = [1]
+
         for i in range(0, len(data)):
             # prepend 1 to array of X values for easy computations
             full_data = [1, data[i]]
             self.apply(full_data, result[i])
 
     def apply(self, row_data, row_result):
-        deltas = []
+
         full_data = [1]
         for i in range(0, len(row_data)):
             full_data.append(row_data[i])
+        deltas = []
         # find out classifier's errors
+        predicted_value = self.predict(full_data)
         for i in range(0, self.size):
-            deltas.append(full_data[i]*(1 / (self.period * self.grad_step)) *
-                          (np.sum(np.multiply(self.params, full_data)) - row_result))
+            self.params[i] = self.params[i] - full_data[i] * ((predicted_value - row_result) * self.grad_step / self.period)
         self.period += 1
-        # apply the errors
-        for i in range(0, self.size):
-            self.params[i] -= deltas[i]
 
     # returns MSE of current classifier according to input values
     def get_error(self, row_data, row_result):
         math_row_data = [1]
         for i in range(0, len(row_data)):
             math_row_data.append(row_data[i])
-        return math.pow((np.sum(np.multiply(self.params, math_row_data)) - row_result), 2) / len(row_data)
+        return self.predict(math_row_data) - row_result
 
     def predict(self, row_data):
+        for i in range(0, len(row_data)):
+            if np.isnan(row_data[i]):
+                row_data[i] = 1
         return np.sum(np.multiply(self.params, row_data))
